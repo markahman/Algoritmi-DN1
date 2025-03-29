8bbcd9aee2f3af4cb61784a249604e405103a029
markahman <mark.ahman1@gmail.com>
2025-03-29 17:13:55 +0100

    Added function for reading data from file

--- Algoritmi-DN1/Algoritmi-DN1.cpp
+++ Algoritmi-DN1/Algoritmi-DN1.cpp
@@ -1,14 +1,54 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <fstream>
+
+std::vector<int> readData(std::string fileName)
+{
+	std::vector<int> data;
+	std::ifstream file(fileName);
+	if (!file.is_open())
+	{
+		std::cerr << "Napaka pri odpiranju datoteke!\n";
+		return data;
+	}
+	int temp;
+	while (file >> temp)
+		data.push_back(temp);
+	file.close();
+	return data;
+}
 
 int main(int argc, char* argv[])
 {
-	if (argc != 2)
+	if (argc != 3)
 	{
-		std::cerr << "Uporaba: " << argv[0] << " <vhodna_datoteka>\n";
+		std::cerr << "Nepravilno stevilo argumentov!\nUporaba: " << argv[0] << " <tip_sortiranja> <ime_datoteke>\n\t-tip_sortiranja: 0 = Counting sort, 1 = Roman sort\n\t-ime_datoteke: ime tekstovne datoteke s podatki, npr. vhod.txt\n";
 		return 1;
 	}
-	std::string fileName = argv[1];
+	std::string a1 = argv[1];
+	std::string a2 = argv[2];
+	bool useRomanSort;
+	std::string fileName;
+	try
+	{
+		if (a1 == "0")
+			useRomanSort = false;
+		else if (a1 == "1")
+			useRomanSort = true;
+		else
+			throw std::exception();
+
+		if (a2.substr(a2.size() - 4) == ".txt")
+			fileName = a2;
+		else
+			throw std::exception();
+	}
+	catch (std::exception e)
+	{
+		std::cerr << "Nepravilna uporaba argumentov!\nUporaba: " << argv[0] << " <tip_sortiranja> <ime_datoteke>\n\t-tip_sortiranja: 0 = Counting sort, 1 = Roman sort\n\t-ime_datoteke: ime tekstovne datoteke s podatki, npr. vhod.txt\n";
+		return 1;
+	}
 
 	return 0;
 }
